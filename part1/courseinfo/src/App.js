31bb39d8d4fd39628ac276b1c5de6fe06c3670ce
Sara Olveira <saralouro7@gmail.com>
2023-04-04 19:11:37 +0200

    part 1.a exercises

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,47 @@
+const Header = (props) => {
+  return <h1>{props.course}</h1>
+}
+
+const Part = (props) => {
+  return <p>{props.part} {props.exercises}</p>
+}
+
+const Content = (props) => {
+  return <>
+    <Part part={props.parts[0].part} exercises={props.parts[0].exercises}/>
+    <Part part={props.parts[1].part} exercises={props.parts[1].exercises}/>
+    <Part part={props.parts[2].part} exercises={props.parts[2].exercises}/>
+  </>
+}
+
+const Total = (props) => {
+  return <p>Number of exercises {props.sum}</p>
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts =  [
+    {
+      part: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      part: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      part: 'State of a component',
+      exercises: 14
+    }
+  ]
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total sum={parts[0].exercises + parts[1].exercises + parts[2].exercises}/>
+    </div>
+  )
+}
+
+export default App
