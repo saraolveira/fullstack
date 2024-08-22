654f140113558b724187311c1046f331480636f7
Sara Olveira <saralouro7@gmail.com>
2024-08-22 18:14:04 +0200

    clean

--- part2/prev/phonebook/src/components/People.js
+++
@@ -1,9 +0,0 @@
-import Person from "./Person.js"
-const People = ({persons}) => {
-
-    return <>
-        {persons.map(person => <Person key={person.name} name={person.name} number={person.number}/>)}
-    </>
-}
-
-export default People
--- part2/prev/phonebook/src/components/Person.js
+++
@@ -1,7 +0,0 @@
-const Person = ({name, number}) => {
-    return <>
-    <div>{name} {number}</div>
-    </>
-}
-
-export default Person
