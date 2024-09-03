31e4d42eb4bb28f6232b717a19e75099d9ac24f8
Sara Olveira <saralouro7@gmail.com>
2024-09-03 12:03:17 +0200

    separate app and controllers

---
+++ part4/bloglist/controllers/blogs.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose")
+
+const blogsRouter = require("express").Router()
+const blogSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number,
+})
+
+const Blog = mongoose.model("Blog", blogSchema)
+
+blogsRouter.get("/", (request, response) => {
+    Blog.find({}).then((blogs) => {
+        response.json(blogs)
+    })
+})
+
+blogsRouter.post("/", (request, response) => {
+    const blog = new Blog(request.body)
+
+    blog.save().then((result) => {
+        response.status(201).json(result)
+    })
+})
+
+module.exports = blogsRouter
